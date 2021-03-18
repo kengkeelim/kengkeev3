faf6893267255310a700b9d4328318bd949ef347
kengkeelim <limkengkee88@gmail.com>
2021-03-18 13:15:38 +0800

    Add react-gtm-module library

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-598ZRRZ" });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
