19e755bdd81857ad0fae1f5a84d97e25c1d688f7
kengkeelim <limkengkee88@gmail.com>
2021-03-20 12:19:35 +0800

    Update Thank You page

--- src/pages/tq.js
+++ src/pages/tq.js
@@ -10,20 +10,23 @@ function ThankYou() {
         <meta name="robots" content="noindex" />
       </Head>
 
-      <section className="h-screen grid place-content-center text-center text-gray-800 p-10 md:px-20 md:w-3/4 md:mx-auto">
-        <h1 className="text-4xl font-black md:text-6xl">
-          Thank You For Signing Up My Newsletter.
+      <section className="h-screen grid place-content-center text-center p-10 md:px-20 lg:max-w-screen-md md:mx-auto">
+        <h1 className="font-black text-gray-800 text-4xl md:text-5xl">
+          Thank You For Signing Up For My Newsletter.
         </h1>
         <Link href="/" as="/" replace>
           <a>
             <button
               type="button"
-              className="text-sm bg-orange-light text-white w-full md:w-1/2 font-bold p-3 mt-6 focus:outline-none border-2 hover:bg-white hover:text-orange-light hover:border-orange-light"
+              className="text-sm md:text-xl bg-orange-light text-white w-full md:w-5/6 font-bold p-3 mt-6 md:mt-10 focus:outline-none border-2 hover:bg-white hover:text-orange-light hover:border-orange-light"
             >
               BACK TO MAIN PAGE
             </button>
           </a>
         </Link>
+        <p className="italic text-sm text-orange-light mt-3 md:mt-6">
+          * Please check your email inbox to confirm your subscription.
+        </p>
       </section>
     </div>
   );
