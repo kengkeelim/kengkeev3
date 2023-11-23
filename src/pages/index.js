2b55fe8eee8f006c2677d7fad3f5f0fa135df220
kengkeelim <limkengkee88@gmail.com>
2023-11-23 18:05:32 +0800

    Design change in progress 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 import styles from "../styles/Home.module.css";
 
-import profile from "../assets/KK-Orange.png";
+import profile from "../assets/undraw_hello_re_3evm.svg";
 
 export default function Home() {
   return (
